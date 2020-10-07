0ecc3635be0c34ee6617906c94ce6c497e2f7558
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-08 00:13:44 +0200

    Created the signup and login system from auth v0.0.3

--- movies_site/movies/views.py
+++ movies_site/movies/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'movies/home.html')
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signupview.html'
