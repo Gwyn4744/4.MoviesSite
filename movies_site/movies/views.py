d5179e053ce0277fd2945a47c66d05052967f5cf
Dominik Bednarski <dominikbednarski@wp.pl>
2020-10-09 14:25:35 +0200

    Deleting Videos v0.0.20

--- movies_site/movies/views.py
+++ movies_site/movies/views.py
@@ -61,6 +61,11 @@ def video_search(request):
         return JsonResponse(response.json())
     return JsonResponse({'Hello': 'Not able to validate form'})
 
+class DeleteVideo(generic.DeleteView):
+    model = Video
+    template_name = 'movies/delete_video.html'
+    success_url = reverse_lazy('dashboard')
+
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
